9156455e2b52747ffeaa5ec08550d0f195139b7f
Hasan Joldic <hasan.joldic@enki.ba>
2022-09-08 14:51:25 +0200

    feat: export only transform functions
    BREAKING CHANGE: change in exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-interface SeedStep<T = unknown> {
-  shouldApplyMutation: boolean;
-  data: T[];
-  mutation: (data: T[]) => Promise<void>;
-}
-
-export async function seed(seedSteps: SeedStep[]) {
-  for (const seedStep of seedSteps) {
-    const { shouldApplyMutation, data, mutation } = seedStep;
-    if (shouldApplyMutation) {
-      await mutation(data);
-    }
-  }
-}
+export * from "./transformData";
